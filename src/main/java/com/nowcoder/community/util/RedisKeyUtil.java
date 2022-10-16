527bddb836365630582648ffc633ad57daf0878a
Brett <2043727856@qq.com>
2022-10-16 18:04:08 +0800

    实现网站数据统计‘ git commit -m实现网站数据统计”

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -13,6 +13,8 @@ public class RedisKeyUtil {
   private static final String PREFIX_TICKET = "ticket";//凭证
   private static final String PREFIX_USER = "user";
 
+  private static final String   PREFIX_UV = "uv";
+  private static final String   PREFIX_DAU = "dau";
 
 
 
@@ -59,6 +61,26 @@ public class RedisKeyUtil {
     //用户
     public static String getUserKey(int userId){
         return PREFIX_USER + SPLIT + userId;
+    }
+
+    //单日uv
+    public static String getUVKey(String date){
+        return PREFIX_UV +SPLIT + date;
+    }
+
+    //区间uv
+    public static String getUVKey(String startDate,String endDate){
+        return PREFIX_UV + SPLIT + startDate + SPLIT + endDate;
+    }
+
+    //单日活跃用户
+    public static String getDAUKey(String date){
+        return PREFIX_DAU + SPLIT + date;
+    }
+
+    //区间活跃用户
+    public static String getDAUKey(String startDate,String endDate){
+        return PREFIX_DAU + SPLIT + startDate + SPLIT + endDate;
     }
 
 
